09240ecdb6e2d58b9a7c7d0a1247cbbcf2043706
juniklee <dlrl0326@naver.com>
2023-05-11 10:41:58 +0900

    react-practice4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
+export function App() {
   return <div>이준읶 리액트 처음함</div>;
 }
-function App2() {
+export function App2() {
   return <div>이준읶 리액트 처음함2</div>;
 }
 
-export default App;
+
 
